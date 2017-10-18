c72782a8232d809634f12aa4aca2253b2d38d039
Divya Mehra <divyamehra@divya-mehra-mbp.local>
2017-10-18 13:09:54 -0400

    Scraped people pages and all profiles using Scrapy

--- scraper/scraper/spiders/people_spider.py
+++ scraper/scraper/spiders/people_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 
 class PeopleSpider(scrapy.Spider):
@@ -15,8 +16,44 @@ class PeopleSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'people-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+
+         for person in response.css('article.person'):
+            yield {
+                'profilelink': person.css('a.person-link').extract(),
+                'name': person.css('h1.person-name').extract()
+            }
+
+class ProfileSpider(scrapy.Spider):
+    name = "profile"
+
+    with open("people.json", 'r') as infile:
+        contents = json.load(infile)
+
+        start_urls = []
+        for item in contents:
+            profile = str(item['profilelink'])
+            profile = "https://creativemornings.com" + profile.partition("href=")[2].partition(">View")[0].strip('\"')
+            start_urls.append(profile)
+
+
+    def parse(self, response):
+
+        for profile in response.css('div.ariaMain'):
+
+            yield {
+            'name': profile.css('a.user-name-inner::text').extract(),
+            'city': profile.css('div.chapter-box::text').extract_first(),
+            'picture': profile.css('img.avatar-bg::attr(src)').extract(),
+            'desc': profile.css('div.user-details::text').extract(),
+            'links': profile.css('div.has-sites a::attr(href)').extract(),
+            'social': profile.css('a.social-link::attr(href)').extract(), # unsure if grabs 1 
+            'bio': profile.css('div.user-bio').extract(),
+            'watchlater': profile.css('a.profile-stat>strong::text').extract()[0],
+            'attended': profile.css('a.profile-stat>strong::text').extract()[1],
+            'skills': profile.css('div.user-skill-bubbles a::text').extract(),
+            'uploads': profile.css('article.gallery-item::attr(data-item)').extract(),
+            'questions': profile.css('article.user-answer label.cm-label::text').extract(),
+            'answers': profile.css('article.user-answer div.answer-text>p').extract(),
+            'qa': profile.css('article.user-answer').extract(),
+            'activity': profile.css('article.iso-item a.talk-text::attr(href)').extract()
+            }
